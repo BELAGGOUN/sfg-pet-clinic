b7e60e6841cd762a982ebf11cdf285dadb622850
BELAGGOUN <belaggoun2000@yahoo.fr>
2019-07-28 23:09:23 -0400

    Create visit map service. Closes #48

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerMapService.java
@@ -39,11 +39,11 @@ public class OwnerMapService extends AbstractMapService<Owner,Long> implements  
     }
 
     @Override
-    public Owner save(Owner object) {
-
-        if(object != null){
-            if (object.getPets() != null) {
-                object.getPets().forEach(pet -> {
+    public Owner save(Owner owner)
+    {
+        if(owner != null){
+            if (owner.getPets() != null) {
+                owner.getPets().forEach(pet -> {
                     if (pet.getPetType() != null){
                         if(pet.getPetType().getId() == null){
                             pet.setPetType(petTypeService.save(pet.getPetType()));
@@ -59,7 +59,7 @@ public class OwnerMapService extends AbstractMapService<Owner,Long> implements  
                 });
             }
 
-            return super.save(object);
+            return super.save(owner);
 
         } else {
             return null;
